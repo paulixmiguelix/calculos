6b3f61d252be59a47dda66a2b4c2f57e3fb1c4b0
Paulo Marques <4985614+paulixmiguelix@users.noreply.github.com>
2023-10-08 16:04:41 +0100

    Acrescentado código para testar

--- Program.cs
+++ Program.cs
@@ -13,6 +13,10 @@ namespace Calculos
             Console.WriteLine("Aplicação Cálculos");
             Console.WriteLine($"4+2={Aritmetica.Somar(4,2)}");
             Console.WriteLine($"4-2={Aritmetica.Subtrair(4, 2)}");
+            double t1 = Conversoes.ConverteTemperatura(Conversoes.ConvercaoTemperatura.CelciusFahr, 36);
+            double t2 = Conversoes.ConverteTemperatura(Conversoes.ConvercaoTemperatura.FahrCelcius, 100);
+            Console.WriteLine($" 36º Celcius={t1}º Fahrenheit");
+            Console.WriteLine($" 100º Fahrenheit={t2}º Celcius");
             Console.ReadKey();
         }
     }
